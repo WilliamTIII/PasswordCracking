8ff377f15e910ba65585663da1e965e755d96a71
Steve <steve.tollaksen@gmail.com>
2020-01-16 09:47:58 -0700

    added code to loop through multiple users having the same password

--- combinenameswithpasswords.ps1
+++ combinenameswithpasswords.ps1
@@ -1,11 +1,38 @@
-﻿$hashes=get-content d:\output\ntlm-hashes.ntds
-$pass=get-content d:\output\cracked.out
+﻿#python -m SimpleHTTPServer
+#copy the files into one dir, then run this. connect to kali IP:8000
+
+$hashes=get-content C:\steve\ntds\ntlm-extract.ntds
+$pass=get-content C:\steve\ntds\cracked.out
+
+Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
+
 
 $out=@()
+
+
 foreach($password in $pass){
-    $user = (get-aduser (($hashes | select-string -pattern ($password.split(":")[0])).tostring().split(":")[0]).split("\")[1]).userprincipalname
-    $passwd=$password.split(":")[1]
-    $out+=New-Object psObject -Property @{'User'=$user;'Password'= $passwd}
+    $passwordlocation=($hashes | select-string -pattern ($password.split(":")[0]))
+    if($passwordlocation.count -gt 1){
+        foreach($individual in $passwordlocation){
+            $user = (get-aduser ($individual.tostring().split(":")[0]).split("\")[1]).userprincipalname
+            $passwd=$password.split(":")[1]
+            $passresult=($DS.ValidateCredentials($user, $passwd))
+            $out+=New-Object psObject -Property @{'User'=$user;'Password'= $passwd;'CurrentPasswd'=$passresult}
+        }
+        continue
+    }
+    else{
+        $user = (get-aduser ($passwordlocation.tostring().split(":")[0]).split("\")[1]).userprincipalname
+        $passwd=$password.split(":")[1]
+        $passresult=($DS.ValidateCredentials($user, $passwd))
+        $out+=New-Object psObject -Property @{'User'=$user;'Password'= $passwd;'CurrentPasswd'=$passresult}
+        continue
+    }
+    
 }
 
+
+
 $out
+
