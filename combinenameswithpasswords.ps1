2d5b551b37d6d241d6ff674bbdd4733530b7f26e
Steve <steve.tollaksen@gmail.com>
2020-01-16 07:04:39 -0700

    turns the original ntlm hashes with the output file to give you username and cleartext password

---
+++ combinenameswithpasswords.ps1
@@ -0,0 +1,11 @@
+﻿$hashes=get-content d:\output\ntlm-hashes.ntds
+$pass=get-content d:\output\cracked.out
+
+$out=@()
+foreach($password in $pass){
+    $user = (get-aduser (($hashes | select-string -pattern ($password.split(":")[0])).tostring().split(":")[0]).split("\")[1]).userprincipalname
+    $passwd=$password.split(":")[1]
+    $out+=New-Object psObject -Property @{'User'=$user;'Password'= $passwd}
+}
+
+$out
